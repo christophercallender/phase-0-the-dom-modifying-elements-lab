1f9a2de679b5348fe3fd37db55ee4790b3cd53ca
Christopher Callender <christophercallender0000@gmail.com>
2022-04-24 12:15:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-document.getElementById("main").remove();
+document.getElementById('main').remove();
 
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id", "victory");
-newHeader.innerHTML = "Christopher is the champion";
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Christopher is the champion';
+
+const test = 1 + 1;
